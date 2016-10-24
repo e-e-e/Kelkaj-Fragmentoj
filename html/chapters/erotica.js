96fe43196d52dccbde48d0aa01e3348ec72520ac
e-e-e <e-e-e@users.noreply.github.com>
2016-10-24 22:36:01 +1100

    add chapter about erotica

---
+++ html/chapters/erotica.js
@@ -0,0 +1,229 @@
+if(!document.chapters) {
+	document.chapters = [];
+}
+
+document.chapters.push({
+	title:'erotica',
+	script:[
+		{
+		index:1,
+		en:'There is a story that Umberto Eco once said to a crowded lecture hall ',
+		eo:'Estas anekdoto, ke Umberto Eco iam diris al homplena aŭditorio'
+	}, 
+	{
+		index:2,
+		en:'that Esperanto wasn’t a real language.',
+		eo:'ke Esperanto ne estas reala lingvo.'
+	},
+	{
+		index:3,
+		en:'He said Esperanto is not a real language:',
+		eo:'li asetis, ke Esperanto ne estas reala lingvo:'
+	},
+	{
+		index:4,
+		en:'because you can’t make love in Esperanto.',
+		eo:'ĉar oni ne povas amori per Esperanto.'
+	}, 
+	{
+		index:5,
+		en:'The story continues, ',
+		eo:'La rakonto daŭras jene'
+	},
+	{
+		index:6,
+		en:'a girl later wrote to him to say,',
+		eo:'virino poste skribis al li por diri, '
+	},
+	{
+		index:7,
+		en:'<em>I’m sorry, Professor Eco, </em>',
+		eo:'<em>Mi pardonpetas, profesoro Eco,</em>'
+	},
+	{
+		index:8,
+		en:'<em>but it is possible to make love in Esperanto. </em>',
+		eo:'<em>Sed amoreblas je Esperanto.</em>'
+	},
+	{
+		index:9,
+		en:'<em>I’ve done it.</em>',
+		eo:'<em>Tion mi faris.</em>'
+	},
+	{
+		index:10,
+		en:'As a planned language, ',
+		eo:'Kiel planlingvo,'
+	},
+	{
+		index:11,
+		en:'originally designed for world peace, ',
+		eo:'unue, kreita por mondpaco,'
+	},
+	{
+		index:12,
+		en:'Esperanto arguably was initially incomplete.',
+		eo:'Esperanto disputeble estis komence nekompleta.'
+	},
+	{
+		index:13,
+		en:'It didn’t contain expletives, profanities, or obscenities of any sort.',
+		eo:'Ĝi tute ne enhavis fivortojn, sakrojn aŭ obscenaĵojn.'
+	},
+	{
+		index:14,
+		en:'The baseness of the bodily functions were not a priority.',
+		eo:'La krudeco de la korpaj funkcioj ne estis prioritato.'
+	},
+	{
+		index:15,
+		en:'Zamenhof had higher goals.',
+		eo:'Zamenhof havis pli gravajn celojn.'
+	},
+	{
+		index:16,
+		en:'But a language without the ability to argue,',
+		eo:'Sed lingvo sen kapablo argumenti, '
+	},
+	{
+		index:17,
+		en:'without the body, without sex,',
+		eo:'sen la korpo, sen amoro,'
+	},
+	{
+		index:18,
+		en:'was really not a language.',
+		eo:'ne estis vere lingvo. '
+	},
+	{
+		index:19,
+		en:'This was the case, but not for long. ',
+		eo:'Tio estis la stato, sed ne delonge. '
+	},
+	null,
+	{
+		index:20,
+		en:'<em>Generally, during orgasm, he forgets everything,</em>',
+		eo:'<em>Ĝenerale, dum orgasmo, Ii forgesis ĉion, </em>'
+	},
+	{
+		index:21,
+		en:'<em>changed into pure joy.</em>',
+		eo:'<em>ŝanĝite al pura ĝuado. </em>'
+	},
+	{
+		index:22,
+		en:'<em>In similar occasions, he usually did not think,</em>',
+		eo:'<em>En similaj okazoj, Ii kutime ne pensis,</em>'
+	},
+	{
+		index:23,
+		en:'<em>did not perceive his surroundings,</em>',
+		eo:'<em>ne perceptis sian ĉirkaŭaĵon, </em>'
+	},
+	{
+		index:24,
+		en:'<em>did not remember who he was with,</em>',
+		eo:'<em>ne memoris kun kiu Ii estas, </em>'
+	},
+	{
+		index:25,
+		en:'<em>only sensed a kind of giant earthquake,</em>',
+		eo:'<em>nur sensis specon de giganta tertremo,</em>'
+	},
+	{
+		index:26,
+		en:'<em>in which his entire life was transformed</em>',
+		eo:'<em>en kiu lia tuta vivo transformiĝis</em>'
+	},
+	{
+		index:27,
+		en:'<em>to wave after wave of extreme pleasure.</em>',
+		eo:'<em>al ondo post ondo de ĝuo intensega. </em>'
+	},
+	{
+		index:28,
+		en:'<em>Mentally, spiritually, he no longer existed,</em>',
+		eo:'<em>Mense, spirite, Ii ne plu ekzistis, </em>'
+	},
+	{
+		index:29,
+		en:'<em>his individuality vanished and also</em>',
+		eo:'<em>lia individueco forsvenis kaj ankaŭ</em>'
+	},
+	{
+		index:30,
+		en:'<em>almost the whole body,</em>',
+		eo:'<em>preskaŭ la tuta korpo,</em>'
+	},
+	{
+		index:31,
+		en:'<em>he somehow melted into the whole universe, but, </em>',
+		eo:'<em>Ii iel fandiĝis kun la tuta kosmo, sed,</em>'
+	},
+	{
+		index:32,
+		en:'<em>merged, the whole universe and</em>',
+		eo:'<em>kunfanditaj, la tuta kosmo kaj </em>'
+	},
+	{
+		index:33,
+		en:'<em>he amounted only to the awareness of a vibrant pillar of pleasurable spit.  </em>',
+		eo:'<em>Ii sumiĝis nur al la konscio esti vibra kolono plezurkraĉa. </em>'
+	},
+	{
+		index:34,
+		en:'<em>He was Life.</em>',
+		eo:'<em>Li estis la Vivo.</em>'
+	},
+	{
+		index:35,
+		en:'<em>But not this time.</em>',
+		eo:'<em>Sed ne ĉi-foje. </em>'
+	},
+	{
+		index:36,
+		en:'<em>Probably because the proposition just made tempted him more and more;</em>',
+		eo:'<em>Verŝajne ĉar lin pli kaj pli tentis la propono ĵus farita; </em>'
+	},
+	{
+		index:37,
+		en:'<em>he confusing half thought,</em>',
+		eo:'<em>Ii konfuze duonpensis, </em>'
+	},
+	{
+		index:38,
+		en:'<em>that it would be best not to cover</em>',
+		eo:'<em>ke estus preferinde ne tro tegi </em>'
+	},
+	{
+		index:39,
+		en:'<em>the precious objects in this room with his discharge.</em>',
+		eo:'<em>per sia erupciaĵo la altvalorajn objektojn en ĉi tiu cambro. </em>'
+	},
+	{
+		index:40,
+		en:'<em>The first spray indeed only made a spot</em>',
+		eo:'<em>La unua ŝpruco fakte faris nur makulon </em>'
+	},
+	{
+		index:41,
+		en:'<em>on the expensive oriental tapestry,</em>',
+		eo:'<em>sur la riĉa orienta tapiŝo, </em>'
+	},
+	{
+		index:42,
+		en:'<em>but the second, more vigorous, </em>',
+		eo:'<em>sed la dua, pli vigla, </em>'
+	},
+	{
+		index:43,
+		en:'<em>directly hit an abstract painting hanging on the wall,</em>',
+		eo:'<em>trafis rekte en murfiksitan abstraktan pentraĵon, </em>'
+	},
+	{
+		index:44,
+		en:'<em>which was possibly worth alot.</em>',
+		eo:'<em>kiu eble multe valoris. </em>'
+	}]
+});
