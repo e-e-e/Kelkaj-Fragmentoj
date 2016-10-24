68c532bc0e36c9f5cc95b73f0aea6969baf16a0b
e-e-e <e-e-e@users.noreply.github.com>
2016-10-24 22:35:50 +1100

    add chapter about collectivity

---
+++ html/chapters/collective.js
@@ -0,0 +1,118 @@
+if(!document.chapters) {
+	document.chapters = [];
+}
+
+document.chapters.push({
+	title: 'collective',
+	script:[
+	{
+		index:1,
+		en:'One of my strongest assumptions about Esperanto was',
+		eo:'Unu el miaj plej fortaj antaŭjuĝoj pri Esperanto'
+	},
+	{
+		index:2,
+		en:'that being a constructed language,',
+		eo:'estis tio, ke planlingvo'
+	},
+	{
+		index:3,
+		en:'it was self evident that its scope of expression would necessarily be limited. ',
+		eo:'evidente havus limigitan amplekson de esprimpovo.'
+	},
+	{
+		index:4,
+		en:'The construction of one man cannot begin to capture that ',
+		eo:'Lingvo verkita de sola viro nepre ne povus kapti tion'
+	},
+	{
+		index:5,
+		en:'which has been forged over aeons ',
+		eo:'kion eonoj forĝis'
+	},
+	{
+		index:6,
+		en:'by the collective struggle towards communication.',
+		eo:'per sia kolektiva lukto al komunikado.'
+	},
+	{
+		index:7,
+		en:'The very idea of a singularly constructed language is ',
+		eo:'La ideo de memfarita lingvo estas,'
+	},
+	{
+		index:8,
+		en:'in my opinion the perfect example of the hubris of man.',
+		eo:'Miaopinie, la perfekta ekzemplo de la senhonteco de homo.'
+	},
+	{
+		index:9,
+		en:'I still hold this belief, ',
+		eo:'Mi ankoraŭ tenas tiun kredon,'
+	},
+	{
+		index:10,
+		en:'but learnt that Esperanto was not really just the creation of Zamenhof.',
+		eo:'sed frue mi lernis, ke Esperanto ne estas la kreaĵo de nur Zamenhof.'
+	},
+	{
+		index:11,
+		en:'Its much more interesting and complex than that. ',
+		eo:'Ĝi estas multe pli interesa kaj kompleksa ol tio.'
+	},
+	{
+		index:12,
+		en:'Zamenhof did devise the basic grammar. ',
+		eo:'Zamenhof ja fiksis la bazan gramatikon.'
+	},
+	{
+		index:13,
+		en:'However, ',
+		eo:'Tamen,'
+	},
+	{
+		index:14,
+		en:'in a move which mirrors the contemporary ethos of the open source community,',
+		eo:'Laŭ stilo, kiu spegulas la nunan etoson de la malfermitkoda komunumo,'
+	},
+	{
+		index:15,
+		en:'he released his work into the wild, ',
+		eo:'li liberigis sian verkon al la mondo,'
+	},
+	{
+		index:16,
+		en:'renouncing all claims to authorship.',
+		eo:'Kaj rezignis ĉian aserton super ĝi.'
+	},
+	{
+		index:17,
+		en:'From very early on Esperanto has been a collective developed; ',
+		eo:'Ekde tre frue, Esperanto kolektive evoluis;'
+	},
+	{
+		index:18,
+		en:'there is no single authority.',
+		eo:'ne ekzistas ununura aŭtoritato.'
+	},
+	{
+		index:19,
+		en:'There is no clear line to be drawn between ',
+		eo:'Ne estas klare distingebla linio inter'
+	},
+	{
+		index:20,
+		en:'supposedly natural languages and those constructed. ',
+		eo:'supozeble naturaj lingvoj kaj konstruitaj.'
+	},
+	{
+		index:21,
+		en:'Look no further than Finnish, or Korean,',
+		eo:'Rigardu la finnan aŭ la korean,'
+	},
+	{
+		index:22,
+		en:'or even Hebrew for evidence.',
+		eo:'aŭ eĉ hebrean por pruvo de tio.'
+	}]
+});
