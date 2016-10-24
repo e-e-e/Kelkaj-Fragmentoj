8e72ae05dc79e9341a554048069a9c6c9604151c
e-e-e <e-e-e@users.noreply.github.com>
2016-10-24 22:36:10 +1100

    add intro

---
+++ html/chapters/intro.js
@@ -0,0 +1,319 @@
+if(!document.chapters) {
+	document.chapters = [];
+}
+
+document.chapters.push({
+	title:'introduction',
+	script:[
+	{
+		index:1,
+		en:'I did not really know much about Esperanto before',
+		eo:'Mi ne vere scias multe pri Esperanto antaŭ ol'
+	},
+	{
+		index:2,
+		en:'Performance Space introduced me to the Esperanto House. ',
+		eo:'Performance Space prezentis al mi la Esperanto-Domon.'
+	},
+	{
+		index:3,
+		en:'This work is the result of four months learning Esperanto. ',
+		eo:'Ĉi tiu artaĵo estas la rezulto de kvar monatoj de Esperanto-studado.'
+	},
+	{
+		index:4,
+		en:'It is intended to demonstrate how easy it is to learn, ',
+		eo:'Ĝi intencas montri, kiel facile oni povas lerni ĝin,'
+	},
+	{
+		index:5,
+		en:'but also to share some of its complexity ',
+		eo:'sed ankaŭ kunhavigi iom da ties komplekso,'
+	},
+	{
+		index:6,
+		en:'which is concealed behind the knee jerk response to reject it outright. ',
+		eo:'kiu estas kaŝita malantaŭ la aŭtomata reago de tuja malakcepto.'
+	},
+	{
+		index:7,
+		en:'Esperanto and this house have a long history. ',
+		eo:'Esperanto kaj ĉi tiu domo havas longan historion.'
+	},
+	{
+		index:8,
+		en:'It is worthy of your time.',
+		eo:'Ĝi meritas vian tempon.'
+	},
+	null,
+	{
+		index:9,
+		en:'Esperanto was first devised around 1887 by Ludwik Zamenhof.',
+		eo:'Esperano unue eldoniĝis en 1887 de Ludwik Zamenhof.'
+	},
+	{
+		index:10,
+		en:'It was developed to be an easy to learn second language. ',
+		eo:'Ĝi estis kreita por esti facile lernebla dua lingvo. '
+	},
+	{
+		index:11,
+		en:'As such, it follows a very simple and importantly regular grammatical structure.',
+		eo:'Do, ĝi havas tre simplan kaj plej grave regulan gramatikan strukturon. '
+	},
+	{
+		index:12,
+		en:'It was never intended to be perfect. ',
+		eo:'Ĝi neniam celis esti perfekta lingvo.'
+	},
+	{
+		index:13,
+		en:'There have been other attempts to create perfectly logical languages ',
+		eo:'Estis aliaj provoj krei perfektajn logikajn lingvojn.'
+	},
+	{
+		index:14,
+		en:'— Lojban or Ithkuil for example — ',
+		eo:'— ekzemple, Loĵbano aŭ Ifkuilo —'
+	},
+	{
+		index:15,
+		en:'aiming to reduce or remove ambiguity entirely. ',
+		eo:'Kiuj celis maloftigi aŭ entute forigi ambiguecon.'
+	},
+	{
+		index:16,
+		en:'But, much like Borge’s Funes the Memorious,',
+		eo:'Sed, samkiel Funes the Memorious de la aŭtoro Borge,'
+	},
+	{
+		index:17,
+		en:'who was crippled by his wondrous ability  ',
+		eo:'Kiu estis kripligita per sia mirindega kapablo '
+	},
+	{
+		index:18,
+		en:'to remember every detail of everything.',
+		eo:'Por memori ĉiun detalon de ĉio.'
+	},
+	{
+		index:19,
+		en:'The detail and specificity of such languages renders them useless. ',
+		eo:'La detaleco kaj specifeco de tiaj lingvoj igas ilin senutilaj.'
+	},
+	{
+		index:20,
+		en:'Esperanto in contrast is pragmatic. ',
+		eo:'Kontraste, Esperanto estas pragmata.'
+	},
+	{
+		index:21,
+		en:'Zamenhof’s idea, which he shared, ',
+		eo:'La ideo de Zamenhof, kiun li prezentis,'
+	},
+	{
+		index:22,
+		en:'was of a united world; ',
+		eo:'estis de unuiĝinta mondo;'
+	},
+	{
+		index:23,
+		en:'united by a common language. ',
+		eo:'unuigita per komuna lingvo.'
+	},
+	{
+		index:24,
+		en:'The idea of a universal language made me uncomfortable. ',
+		eo:'La ideo de universala lingvo igis min malkomforta.'
+	},
+	{
+		index:25,
+		en:'I thought about linguistic imperialism ',
+		eo:'Ĝi pensigas min pri lingva imperiismo'
+	},
+	{
+		index:26,
+		en:'— and of some sort of Orwellian Newspeak —',
+		eo:'— kaj pri ia Novparolo de Orwell —'
+	},
+	{	
+		index:27,
+		en:'— but a moment to one side —',
+		eo:'— sed atendu momenton —'
+	},
+	{
+		index:28,
+		en:'I am reminded of the Pirahã language ',
+		eo:'Mi estas rememorigita pri la Pirahana linvgo'
+	},
+	{
+		index:29,
+		en:'— a language from the Amazon basin.',
+		eo:'— lingvo el la Amazona Baseno.'
+	},
+	{
+		index:30,
+		en:'It has been touted as the language of happiness ',
+		eo:'Ĝin laŭdis kiel la lingvon de feliĉeco '
+	},
+	{
+		index:31,
+		en:'by the linguist Daniel Everett. ',
+		eo:'la lingvisto Daniel Everett.'
+	},
+	{
+		index:32,
+		en:'He postulated that its grammar, ',
+		eo:'Li argumentis, ke ĝia gramatiko,'
+	},
+	{
+		index:33,
+		en:'which precludes the use of future and past tense, ',
+		eo:'kiu ne utiligas tensojn por futuro kaj preterito,'
+	},
+	{
+		index:34,
+		en:'caused happiness as its speakers necessarily live in the present.',
+		eo:'Feliĉigas ĉar ĝiaj parolantoj ĉiam vivas en la nuno.'
+	},
+	{
+		index:35,
+		en:'They are unable to worry about the future,',
+		eo:'Ili ne kapablas zorgi pri la estonteco,'
+	},
+	{
+		index:36,
+		en:'or dwell on the past. ',
+		eo:'aŭ pensi pri la estinteco.'
+	},
+	{
+		index:37,
+		en:'This verges on linguistic determinism —',
+		eo:'Ĉi tio proksimiĝas al lingva determinismo —'
+	},
+	{
+		index:38,
+		en:'the view that our thoughts are wholly shaped by language. ',
+		eo:'La opinio, ke niajn pensojn tute formigas nia lingvo.'
+	},
+	{
+		index:39,
+		en:'It is obviously not totally true.',
+		eo:'Evidente ne estas tute ĝusta.  '
+	},
+	{
+		index:40,
+		en:'But, what type of thinking does esperanto facilitate? ',
+		eo:'Sed, kian pensadon Esperanto faciligas?'
+	},
+	{
+		index:41,
+		en:'… The utopic.',
+		eo:'… la utopiismon.'
+	},
+	{
+		index:42,
+		en:'Esperanto, the name itself, translates to one who hopes. ',
+		eo:'Esperanto, la nomo mem, tradukiĝas kiel iu kiu esperas.'
+	},
+	{
+		index:43,
+		en:'<em>To the world eternally warring </em>',
+		eo:'<em>Al la mond’ eterne militanta</em>'
+	},
+	{
+		index:44,
+		en:'<em>It promises sacred harmony.</em>',
+		eo:'<em>Ĝi promesas sanktan harmonion.</em>'
+	},
+	{
+		index:45,
+		en:'or ',
+		eo:'aŭ'
+	},
+	{
+		index:46,
+		en:'<em>The most happiness to the most people.</em>',
+		eo:'<em>La pleja feliĉo por la plejo da homoj.</em>'
+	},
+	{
+		index:47,
+		en:'It’s really quite sad, ',
+		eo:'Estas vere malfeliĉe,'
+	},
+	{
+		index:48,
+		en:'that Pirahã and other minority languages',
+		eo:'ke Pirahana kaj aliaj minoritataj lingvoj'
+	},
+	{
+		index:49,
+		en:'are currently under a real threat of extinction. ',
+		eo:'estas nuntempe minacataj de formorto.'
+	},
+	{
+		index:50,
+		en:'The hegemony of dominant cultures deplore such difference. ',
+		eo:'La hegemonio de ĉefaj kulturoj malamas tian diferencon.'
+	},
+	{
+		index:51,
+		en:'Esperanto in contrast is a living language, ',
+		eo:'Kontraste, Esperanto estas vivanta lingvo,'
+	},
+	{
+		index:52,
+		en:'with native speakers ',
+		eo:'kun denaskuloj'
+	},
+	{
+		index:53,
+		en:'and a growing group of speakers. ',
+		eo:'kaj kun kreskanta grupo de parolantoj.'
+	},
+	{
+		index:54,
+		en:'Despite first appearances, ',
+		eo:'Malgraŭ unuaj ŝajnoj,'
+	},
+	{
+		index:55,
+		en:'it does not seek to replace other languages. ',
+		eo:'ĝi ne intencas anstataŭi aliajn lingvojn.'
+	},
+	{
+		index:56,
+		en:'For example the Universal Esperanto Association (UEA)',
+		eo:'Ekzemple, la Universala Esperanto-Asocio (UEA)'
+	},
+	{
+		index:57,
+		en:'holds multilingualism, language rights, and language diversity',
+		eo:'konsideras multlingvecon, lingvajn rajtojn kaj lingvan diversecon'
+	},
+	{
+		index:58,
+		en:'amongst its core objectives.',
+		eo:'inter ĝiaj kernaj celoj.'
+	},
+	{
+		index:59,
+		en:'Esperanto is at home in our globalised world, ',
+		eo:'Esperanto estas hejme en nia tutmondigita globo,'
+	},
+	{
+		index:60,
+		en:'but offers an alternative to the homogeneity of English Imperialism.',
+		eo:'sed ofertas alternativon al la homogeneco de Angla imperiismo.'
+	},
+	{
+		index:61,
+		en:'In a sense it was made for this time, ',
+		eo:'Iusence, ĝi estis kreita por ĉi tiu epoko,'
+	},
+	{
+		index:62,
+		en:'but perhaps arrived a bit too early. ',
+		eo:'sed eble enmondiĝis iom tro frue.'
+	}]
+});
