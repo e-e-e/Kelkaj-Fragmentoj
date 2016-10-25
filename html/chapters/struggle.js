c910f0d11c2a18e1f46aa291202494a5276558ba
e-e-e <e-e-e@users.noreply.github.com>
2016-10-25 21:44:13 +1100

    add two more chapters

---
+++ html/chapters/struggle.js
@@ -0,0 +1,530 @@
+if(!document.chapters) {
+	document.chapters = [];
+}
+
+document.chapters.push({
+	title:'struggle',
+	script:[{
+		index:1,
+		en:'One night I attempted to read ',
+		eo:'Unu nokton mi penis legi'
+	},
+	{
+		index:2,
+		en:'‘The storm’ by John S. Dinwoodie.',
+		eo:'‘La ŝtormon’ fare de John S. Dinwoodie.'
+	},
+	{
+		index:3,
+		en:'This short story originally written in esperanto, ',
+		eo:'Ĉi tiu novelo, originale verkita en Esperanto,'
+	},
+	{
+		index:4,
+		en:'was surprisingly difficult to read. ',
+		eo:'Estis surprize malfacile legi.'
+	},
+	{
+		index:5,
+		en:'Every second word forced me to consult the dictionary. ',
+		eo:'Ĉiu dua vorto devigis min konsulti la vortaron.'
+	},
+	null,
+	{
+		index:6,
+		en:'<em>It is very strange,</em>',
+		eo:'<em>Estas tre strange, </em>'
+	},
+	{
+		index:7,
+		en:'<em>the manner in which the fisherman, </em>',
+		eo:'<em>kiamaniere la fiŝkaptistoj, </em>'
+	},
+	{
+		index:8,
+		en:'<em>who seeing via the waves,</em>',
+		eo:'<em>kiun vivas per la maro, </em>'
+	},
+	{
+		index:9,
+		en:'<em>becomes aware of, like some sort of telepathy, </em>',
+		eo:'<em>ekkonscias per ia telepatio, </em>'
+	},
+	{
+		index:10,
+		en:'<em>when danger threatens a ship, </em>',
+		eo:'<em>kiam danĝero minacas ŝipon,</em>'
+	},
+	{
+		index:11,
+		en:'<em>their own or anothers other’s.</em>',
+		eo:'<em>propran aŭ fremdan.</em>'
+	},
+	null,
+	{
+		index:12,
+		en:'The Storm is about a man ',
+		eo:'La Ŝtormo estas pri viro'
+	},
+	{
+		index:13,
+		en:'from a small coastal town in Scottland',
+		eo:'de marborda urbeto Skotlanda'
+	},
+	{
+		index:14,
+		en:'who one night wittnesses a ship stuck in a storm.',
+		eo:'kiu unu nokton vidas ŝipon kaptita en ŝtormo.'
+	},
+	{
+		index:15,
+		en:'Watching at first from his window, ',
+		eo:'Rigardante unue el lia fenestro,'
+	},
+	{
+		index:16,
+		en:'he exits his house ',
+		eo:'Li forlasas sian domon '
+	},
+	{
+		index:17,
+		en:'and joins the entire town, ',
+		eo:'kaj kun la tuta urbeto,'
+	},
+	{
+		index:18,
+		en:'<em>who watch the struggle of the fishing boat against the weather.  </em>',
+		eo:'<em>rigardas la lukton de la fiŝa ŝipeto kontraŭ la vetero.</em>'
+	},
+	{
+		index:19,
+		en:'<em>I had left the house </em>',
+		eo:'<em>Mi estis elirinta el la domo </em>'
+	},
+	{
+		index:20,
+		en:'<em>to go to the hill next to the port,</em>',
+		eo:'<em>por iri al la deklivo apud la haveno, </em>'
+	},
+	{
+		index:21,
+		en:'<em>because I knew </em>',
+		eo:'<em>ĉar mi sciis, </em>'
+	},
+	{
+		index:22,
+		en:'<em>that all the villagers will be there</em>',
+		eo:'<em>ke ĉiuj vilaĝanoj estos tie </em>'
+	},
+	{
+		index:23,
+		en:'<em>- not to help, </em>',
+		eo:'<em>- ne por helpi, </em>'
+	},
+	{
+		index:24,
+		en:'<em>because help would be impossible</em>',
+		eo:'<em>ĉar helpo estus neebla</em>'
+	},
+	{
+		index:25,
+		en:'<em>in such a storm like this, </em>',
+		eo:'<em>en tia ventego kia ĉi tiu, </em>'
+	},
+	{
+		index:26,
+		en:'<em>and not to find pleasure in the sight,</em>',
+		eo:'<em>kaj ne por trovi plezuron en la vidotaĵo,</em>'
+	},
+	{
+		index:27,
+		en:'<em>because the view is harrowing, </em>',
+		eo:'<em>ĉar la vidaĵo estas korŝira, </em>'
+	},
+	{
+		index:28,
+		en:'<em>when people struggle</em>',
+		eo:'<em>kiam homoj baraktas </em>'
+	},
+	{
+		index:29,
+		en:'<em>in saving life against </em>',
+		eo:'<em>pro savi la vivon kontraŭ</em>'
+	},
+	{
+		index:30,
+		en:'<em>raging elements,</em>',
+		eo:'<em>furiozantaj elementoj, </em>'
+	},
+	{
+		index:31,
+		en:'<em>but only because they couldn’t stay at home,</em>',
+		eo:'<em>sed nur ĉar ili ne povus resti hejme, </em>'
+	},
+	{
+		index:32,
+		en:'<em>because they would be drawn to the scene</em>',
+		eo:'<em>ĉar ili estus tirataj al la scenejo </em>'
+	},
+	{
+		index:33,
+		en:'<em>as if by a magnet.</em>',
+		eo:'<em>kvazaŭ de magneto.</em>'
+	},
+	{
+		index:34,
+		en:'Here I was, ',
+		eo:'Jen mi,'
+	},
+	{
+		index:35,
+		en:'struggling to read,',
+		eo:'mi luktis legi,'
+	},
+	{
+		index:36,
+		en:'to understand these words,',
+		eo:'kaj kompreni tiujn vortojn,'
+	},
+	{
+		index:37,
+		en:'as the man was attempting to understand what he was witnessing.',
+		eo:'samkiel la viro penis kompreni kion li vidis.'
+	},
+	{
+		index:38,
+		en:'He asked the other silent onlookers, ',
+		eo:'Li demandis la aliajn silentajn atestantojn,'
+	},
+	{
+		index:39,
+		en:'trying to grasp what exactly was going on.',
+		eo:'kiuj penas kompreni kion precize okazas.'
+	},
+	{
+		index:40,
+		en:'<em>Then I understood, </em>',
+		eo:'<em>Tiam mi komprenis,</em>'
+	},
+	{
+		index:41,
+		en:'<em>why I felt something unusual  </em>',
+		eo:'<em>kial mi sentis ion nekutiman </em>'
+	},
+	{
+		index:42,
+		en:'<em>in the silence of the crowd.</em>',
+		eo:'<em>en la silento de la homamaso.  </em>'
+	},
+	{
+		index:43,
+		en:'<em>Since I began living in the village, </em>',
+		eo:'<em>De kiam mi ekloĝis en la vilaĝo, </em>'
+	},
+	{
+		index:44,
+		en:'<em>never had a danger befell some of its own ships,</em>',
+		eo:'<em>neniam danĝero trafis iun el la ŝipetoj apartenantaj al ĝi, </em>'
+	},
+	{
+		index:45,
+		en:'<em>but it was one of "our boats,"</em>',
+		eo:'<em>sed tio estis unu el "niaj ŝipetoj",</em>'
+	},
+	{
+		index:46,
+		en:'<em>and its captian was a popular young man </em>',
+		eo:'<em>kaj ĝia estro estis populara junulo, </em>'
+	},
+	{
+		index:47,
+		en:'<em>who was married only two weeks early.</em>',
+		eo:'<em>kiu edziĝis antaŭ nur du semajnoj.</em>'
+	},
+	{
+		index:48,
+		en:'<em>Along with him in the boat, </em>',
+		eo:'<em>Kune kun li en la ŝipeto, </em>'
+	},
+	{
+		index:49,
+		en:'<em>I knew,</em>',
+		eo:'<em>mi sciis,</em>'
+	},
+	{
+		index:50,
+		en:'<em>were his two younger brothers </em>',
+		eo:'<em>estis liaj du plijunaj fratoj </em>'
+	},
+	{
+		index:51,
+		en:'<em>and two cousins:</em>',
+		eo:'<em>kaj du kuzoj: </em>'
+	},
+	{
+		index:52,
+		en:'<em>it was</em>',
+		eo:'<em>ĝi estis, </em>'
+	},
+	{
+		index:53,
+		en:'<em>in our words,</em>',
+		eo:'<em>laŭ nia dirmaniero, </em>'
+	},
+	{
+		index:54,
+		en:'<em>“a family boat"</em>',
+		eo:'<em>"familia ŝipeto".</em>'
+	},
+	{
+		index:55,
+		en:'The group on the hill watched as the ship aproached.',
+		eo:'La grupo sur la monteto rigardis la ŝipon al proksimiĝi.'
+	},
+	{
+		index:56,
+		en:'The entrance to the port was such ',
+		eo:'La enirejo de la haveno estis tia'
+	},
+	{
+		index:57,
+		en:'that even in good weather it was difficult to enter.',
+		eo:'ke eĉ dum bona vetero estus malfacile eniri.'
+	},
+	{
+		index:58,
+		en:'The people watching knew this.',
+		eo:'La rigardantoj scias tion.'
+	},
+	{
+		index:59,
+		en:'<em>And indeed a disaster was</em>',
+		eo:'<em>Kaj ja katastrofo estis</em>'
+	},
+	{
+		index:60,
+		en:'<em>not only possible </em>',
+		eo:'<em>ne nur ebla  </em>'
+	},
+	{
+		index:61,
+		en:'<em>but likely.</em>',
+		eo:'<em>sed verŝajna.</em>'
+	},
+	{
+		index:62,
+		en:'Still some prayed to god, ',
+		eo:'Kelkaj preĝis al dio,'
+	},
+	{
+		index:63,
+		en:'and others worked futilely to help from shore.',
+		eo:'Dum aliaj senefike klopodis helpi ĉe bordo.'
+	},
+	{
+		index:64,
+		en:'<em>It seemed, </em>',
+		eo:'<em>Ŝajnis,</em>'
+	},
+	{
+		index:65,
+		en:'<em>that the boat was approaching more and more rapidly,</em>',
+		eo:'<em>ke la ŝipeto pli kaj pli rapide alproksimiĝas, </em>'
+	},
+	{
+		index:66,
+		en:'<em>until finally we could see the people on the deck.</em>',
+		eo:'<em>ĝis fine ni povis vidi la homojn sur la ferdeko.</em>'
+	},
+	{
+		index:67,
+		en:'<em>All stood holding the helm,</em>',
+		eo:'<em>Ĉiuj staris tenante la direktilon,</em>'
+	},
+	{
+		index:68,
+		en:'<em>ready to move it with all their strength </em>',
+		eo:'<em>pretaj movi ĝin per la tuta forto </em>'
+	},
+	{
+		index:69,
+		en:'<em>immediately when the captain,</em>',
+		eo:'<em>tuj kiam la ŝipestro,</em>'
+	},
+	{
+		index:70,
+		en:'<em>who was standing in middledeck</em>',
+		eo:'<em>kiu staris en la ferdekmezo </em>'
+	},
+	{
+		index:71,
+		en:'<em>judging the distance,</em>',
+		eo:'<em>juĝante la distancon,</em>'
+	},
+	{
+		index:72,
+		en:'<em>gave the order to guide the boat </em>',
+		eo:'<em>donos la ordonon gvidi la ŝipeton </em>'
+	},
+	{
+		index:73,
+		en:'<em>across the waves</em>',
+		eo:'<em>trans la ondojn </em>'
+	},
+	{
+		index:74,
+		en:'<em>at the harbor entrance.</em>',
+		eo:'<em>al la havena enirejo.</em>'
+	},
+	{
+		index:75,
+		en:'<em>It was a vain hope;</em>',
+		eo:'<em>Estis vana espero;</em>'
+	},
+	{
+		index:76,
+		en:'<em>the ship refused to respond</em>',
+		eo:'<em>la ŝipeto rifuzis respondi</em>'
+	},
+	{
+		index:77,
+		en:'<em>to the helm;</em>',
+		eo:'<em>al la direktilo;</em>'
+	},
+	{
+		index:78,
+		en:'<em>It was as if in the ruthless hand of a giant,</em>',
+		eo:'<em>ĝi estis kvazaŭ en la senkompata mano de giganto,</em>'
+	},
+	{
+		index:79,
+		en:'<em>who refused to release it </em>',
+		eo:'<em>kiu rifuzis liberigi ĝin</em>'
+	},
+	{
+		index:80,
+		en:'<em>but drove it irrevocably beyond the entrance.</em>',
+		eo:'<em>sed pelis ĝin nerevokeble preter la enirejon. </em>'
+	},
+	{
+		index:81,
+		en:'<em>While it drifted past the waiting crowd</em>',
+		eo:'<em>Dum ĝi preterdrivis la atendantan homamason </em>'
+	},
+	{
+		index:82,
+		en:'<em>the young captain raised his hand:</em>',
+		eo:'<em>la juna ŝipestro levis la manon:</em>'
+	},
+	{
+		index:83,
+		en:'<em>it seemed to me,  </em>',
+		eo:'<em>ŝajnis al mi, </em>'
+	},
+	{
+		index:84,
+		en:'<em>that he was making not a saluting gesture,</em>',
+		eo:'<em>ke li ne faras salutgeston,</em>'
+	},
+	{
+		index:85,
+		en:'<em>but only unconscious gesture of resignation. </em>',
+		eo:'<em>sed nur senkonscian geston de rezignacio.</em>'
+	},
+	{
+		index:86,
+		en:'On the first reading,',
+		eo:'Dum la unua legado,'
+	},
+	{
+		index:87,
+		en:'I did not at this point realise what had happened.',
+		eo:'Tiam mi ne rimarkis kio okazis'
+	},
+	{
+		index:88,
+		en:'It was not until…',
+		eo:'Ne antaŭ ol …'
+	},
+	{
+		index:89,
+		en:'<em>tears mixed with the raindrops…</em>',
+		eo:'<em>larmoj sin miksis kun la pluveroj…</em>'
+	},
+	{
+		index:90,
+		en:'that i realised what had happened.',
+		eo:'ke mi rimarkis, kio okazis.'
+	},
+	{
+		index:91,
+		en:'<em>We recovered their dead bodies the following morning,</em>',
+		eo:'<em>Ni trovis iliajn kadavrojn en la sekvanta mateno, </em>'
+	},
+	{
+		index:92,
+		en:'<em>when the storm had passed</em>',
+		eo:'<em>kiam forpasis la ŝtormo </em>'
+	},
+	{
+		index:93,
+		en:'<em>when the waves were again tranquil,</em>',
+		eo:'<em>kaj la maro estis denove trankvila, </em>'
+	},
+	{
+		index:94,
+		en:'<em>and we buried five in a tomb</em>',
+		eo:'<em>kaj ni enterigis la kvin en unu tombon</em>'
+	},
+	{
+		index:95,
+		en:'<em>under the shadow of the old church </em>',
+		eo:'<em>sub la ombro de la malnova preĝejo</em>'
+	},
+	{
+		index:96,
+		en:'<em>at the top of the port hill, </em>',
+		eo:'<em>ĉe la supro de la havena deklivo</em>'
+	},
+	{
+		index:97,
+		en:'<em>which looks towards the sea, </em>',
+		eo:'<em>kiu alrigardas al la maro,</em>'
+	},
+	{
+		index:98,
+		en:'<em>where they lived </em>',
+		eo:'<em>sur kiu ili vivis</em>'
+	}, 
+	{
+		index:99,
+		en:'<em>and where they died. </em>',
+		eo:'<em>kaj en kiu ili mortis. </em>'
+	},
+	{
+		index:100,
+		en:'<em>Together they sleep there, </em>',
+		eo:'<em>Kune ili dormas tie,</em>'
+	},
+	{
+		index:101,
+		en:'<em>and over their eternal resting place </em>',
+		eo:'<em>kaj super ilia eterna restadejo</em>'
+	},
+	{
+		index:102,
+		en:'<em>hearing the wild roar of the North Sea</em>',
+		eo:'<em>aŭdiĝas la sovaĝa muĝado de la Norda Maro</em>'
+	},
+	{
+		index:103,
+		en:'<em>and the sad cry of the gulls.</em>',
+		eo:'<em>kaj la malgaja kriado de la mevoj.</em>'
+	},
+	null,
+	{
+		index:104,
+		en:'It is with tears in my eyes that I learnt Esperanto.',
+		eo:'Estis kun larmoj en miaj okuloj, ke mi lernas esperanton.'
+	}]
+});
---
+++ html/chapters/translations.js
@@ -0,0 +1,205 @@
+if(!document.chapters) {
+	document.chapters = [];
+}
+
+document.chapters.push({
+	title:'translations',
+	script:[{
+		index:1,
+		en:'The translation of other languages literature into Esperanto fascinates me. ',
+		eo:'Esperantigo de literaturo el aliaj lingvoj fascinas min.'
+	},
+	{
+		index:2,
+		en:'Translations generally highlight the ideological nature of language. ',
+		eo:'Tradukaĵoj ĝenerale emfazas la ideologian karakteron de lingvo.'
+	},
+	{
+		index:3,
+		en:'Not that language itself is ideological, ',
+		eo:'Ne tion, ke lingvo mem estas ideologia,'
+	},
+	{
+		index:4,
+		en:'but that those who speak a shared language  ',
+		eo:'sed tion, ke parolantoj de la sama lingvo '
+	},
+	{
+		index:5,
+		en:'often share a common base.',
+		eo:'ofte kunhavas komunan moron.'
+	},
+	{
+		index:6,
+		en:'They are exposed to a shared mode of expressing their world. ',
+		eo:'Ili spertas kaj esprimas sian medion per komuna vidpunkto.  '
+	},
+	{
+		index:7,
+		en:'This is how we can speak of german literature,  ',
+		eo:'Jen kiel ni povas paroli pri germana literaturo,  '
+	},
+	{
+		index:8,
+		en:'the english canon,',
+		eo:'pri la angla tekstaro,'
+	},
+	{
+		index:9,
+		en:'etc. ',
+		eo:'k.t.p.'
+	},
+	{
+		index:10,
+		en:'However, Esperanto’s international character means that',
+		eo:'Tamen, la internacia karaktero de Esperanto signifas, ke'
+	},
+	{
+		index:11,
+		en:'you can encounter another culture’s literature ',
+		eo:'vi povas konatiĝi kun la literaturo de alia kulturo'
+	},
+	{
+		index:12,
+		en:'through a lens less tainted by the particularities of your own. ',
+		eo:'per lenso malpli influata de via propra lingvo.'
+	},
+	{
+		index:13,
+		en:'Esperanto is unique, in this sense. ',
+		eo:'Esperanto estas unika, laŭ tiu senco.'
+	},
+	{
+		index:14,
+		en:'It is the only real example of a living language',
+		eo:'Ĝi estas la sola vera ekzemplo de vivanta lingvo'
+	},
+	{
+		index:15,
+		en:'without nationalism at its core. ',
+		eo:'sen naciismo en sia koro.'
+	},
+	{
+		index:16,
+		en:'Its native speakers are united by a shared idea',
+		eo:'Ĝiaj denaskuloj estas unuiĝintaj per komuna ideo '
+	},
+	{
+		index:17,
+		en:'rather than geography.',
+		eo:'anstataŭ per geografio.'
+	},
+	{
+		index:18,
+		en:'Historically this stateless character',
+		eo:'Historie, ĉi tiu sennacieco'
+	},
+	{
+		index:19,
+		en:'made it popular among some anarchists and communists, ',
+		eo:'igis ĝin populara inter iuj anarkiistoj kaj komunistoj,'
+	},
+	{
+		index:20,
+		en:'who saw Esperanto as a tool ',
+		eo:'kiuj vidis Esperanton kiel ilon '
+	},
+	{
+		index:21,
+		en:'to unite the working class across the world. ',
+		eo:'por unuigi la laboristan klason ĉirkaŭ la mondo.'
+	},
+	{
+		index:22,
+		en:'For a time Esperanto was known as the Worker’s Latin.',
+		eo:'Dum iom da tempo, Esperanton oni nomis la latina de la laboristoj.'
+	},
+	null,
+	{
+		index:23,
+		en:'In my opinion, ',
+		eo:'Laŭ mia opinio,'
+	},
+	{
+		index:24,
+		en:'there are a number of contradictions and tensions which animate Esperanto.',
+		eo:'estas iom da kontraŭdiroj kaj tensioj, kiuj ekzistas en Esperanto.'
+	},
+	{
+		index:25,
+		en:'— It is a language born of grammar. ',
+		eo:'— Ĝi estas lingvo, naskita el gramatiko.'
+	},
+	{
+		index:26,
+		en:'And in this sense is a bit back to front. ',
+		eo:'Kaj, laŭ tiu senco, ĝi inverse formiĝis.'
+	},
+	{
+		index:27,
+		en:'Other languages are born of use, ',
+		eo:'Dume, aliaj lingvoj naskiĝis el uzado,'
+	},
+	{
+		index:28,
+		en:'their grammar only coming later ',
+		eo:'iliaj gramatikoj nur stabiliĝis pli poste'
+	},
+	{
+		index:29,
+		en:'to describe and/or regulate use.',
+		eo:'Kaj tiam priskribis kaj/aŭ reguligis uzon.'
+	},
+	null,
+	{
+		index:30,
+		en:'— then — ',
+		eo:'— kaj ankaŭ —'
+	},
+	null,
+	{
+		index:31,
+		en:'Despite the grand aim of universality, ',
+		eo:'Malgraŭ la granda celo de universaleco,'
+	},
+	{
+		index:32,
+		en:'the Esperanto community is actually quite intimate and small. ',
+		eo:'la Esperanto-komunumo estas efektive tre intima kaj malgranda.'
+	},
+	{
+		index:33,
+		en:'The smallness of the community enables a closeness ',
+		eo:'La eteco de la komunumo ebligas intimecon'
+	},
+	{
+		index:34,
+		en:'in spite of physical distance. ',
+		eo:'malgraŭ fizika distanco.'
+	},
+	{
+		index:35,
+		en:'Esperantists open their doors ',
+		eo:'Esperantistoj malfermas sian pordon '
+	},  
+	{
+		index:36,
+		en:'to each other across the globe, ',
+		eo:'al ĉiu samideano, ĉirkaŭ la terglobo,'
+	},
+	{
+		index:37,
+		en:'knowing that they share a common idea.',
+		eo:'Sciante, ke ili kunhavas komunan ideon. '
+	},
+	{
+		index:38,
+		en:'This closeness is only possible',
+		eo:'Tiu intimeco nur estas ebla'
+	},
+	{
+		index:39,
+		en:'because the language is not more widely spoken.',
+		eo:'ĉar la lingvo ne estas vaste parolata.'
+	}]
+});
